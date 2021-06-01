b61c023f632678bffedcbc9ca996f8eb5be1d002
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-01 17:23:56 -0600

    add About test

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About test
+    //first test
+    it('renders', () => {
+        render(<About />);
+    });
+    //second test
+    it('matches snapshot DOM node structure', () => {
+        //render About
+        const { asFragment } = render(<About />);
+        //assert that snapshots match
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
+
